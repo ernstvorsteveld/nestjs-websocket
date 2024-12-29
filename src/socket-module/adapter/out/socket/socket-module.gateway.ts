f7e2e0fc85d3be921c123f0457ca23a53e209fc9
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2024-12-29 16:01:58 +0100

    Remove service for socket

--- src/socket-module/adapter/out/socket/socket-module.gateway.ts
+++ src/socket-module/adapter/out/socket/socket-module.gateway.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -6,28 +7,44 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io-client';
 import { User } from '../../../domain/model/socket-module.user';
-import { SocketService } from './socket-module.socket-service';
+import { Server } from 'socket.io';
+import { WebSocketServer } from '@nestjs/websockets';
 
 @WebSocketGateway()
 export class SocketModuleGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private readonly socketService: SocketService) {}
+  logger = new Logger(SocketModuleGateway.name);
+  private readonly connectedClients: Map<string, Socket> = new Map();
+
+  @WebSocketServer()
+  private readonly server: Server;
+
+  constructor() {}
 
   afterInit() {
-    this.socketService.afterInit();
+    this.logger.log('Initialized');
   }
 
   handleConnection(socket: Socket): void {
-    this.socketService.handleConnection(socket);
+    this.logger.debug(`Client id:${socket.id} connected`);
+    const clientId = socket.id;
+    this.connectedClients.set(clientId, socket);
+
+    socket.on('disconnect', () => {
+      this.connectedClients.delete(clientId);
+    });
   }
 
   handleDisconnect(client: any): void {
-    this.socketService.handleDisconnect(client);
+    this.logger.log(`Cliend id:${client.id} disconnected`);
   }
 
   emitUserCreated(user: User): Promise<void> {
-    this.socketService.emitEvent('user.created', user);
+    // this.logger.debug(`About to emit: ${JSON.stringify(user)}`);
+    // this.socketService.emitEvent('user.created', user);
+    this.logger.debug(`About to emit: ${JSON.stringify(user)}`);
+    this.server.emit('user.created', user);
     return Promise.resolve();
   }
 }
--- src/socket-module/socket-module.module.ts
+++ src/socket-module/socket-module.module.ts
@@ -4,10 +4,6 @@ import {
   UserCreatedListener,
 } from './adapter/in/local-event/socket-module.events';
 import { SocketModuleGateway } from './adapter/out/socket/socket-module.gateway';
-import {
-  SocketService,
-  SocketServiceImpl,
-} from './adapter/out/socket/socket-module.socket-service';
 import { SocketModuleService } from './domain/service/socket-module.service';
 import { EventUseCases } from './port/in/socket-module.usecases';
 
@@ -16,10 +12,6 @@ import { EventUseCases } from './port/in/socket-module.usecases';
     SocketModuleGateway,
     UserCreatedListener,
     UserConverter,
-    {
-      provide: SocketService,
-      useClass: SocketServiceImpl,
-    },
     {
       provide: EventUseCases,
       useClass: SocketModuleService,
